9f4415a1db43342a8c7de9afce635a355d7c60fd
RonyK <sangron@naver.com>
2020-06-18 04:07:59 +0900

    Create Math util class

---
+++ MSDB/MSDB-CORE/include/util/math.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _MSDB_MATH_H_
+#define _MSDB_MATH_H_
+
+#include <cstdint>
+
+namespace msdb
+{
+
+int64_t intDivCeil(int64_t li, int64_t ri);
+
+}
+
+#endif		// _MSDB_MATH_H_
