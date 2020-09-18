4ca3e393868c49d949341dbd46f91794901f71d4
RonyK <sangron@naver.com>
2020-09-18 15:52:40 +0900

    Add astronomy data sets

--- MSDB/MSDB-Test/test/dummy/dummy_astronomy.cpp
+++ MSDB/MSDB-Test/test/dummy/dummy_astronomy.cpp
@@ -55,5 +55,149 @@ void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr)
 	}
 }
 }	// data2D_star1024x1024s
+
+namespace data2D_saturn1024x1024
+{
+std::vector<dim_type> dims = { dimY, dimX };
+std::vector<dim_type> chunkNums = { 8, 8 };
+std::vector<dim_type> chunkDims = { dims[0] / chunkNums[0], dims[1] / chunkNums[1] };
+std::vector<dim_type> blockNums = { 4, 4 };
+std::vector<dim_type> blockDims = { chunkDims[0] / blockNums[0], chunkDims[1] / blockNums[1] };
+
+void getDummy(value_type* output, size_t length)
+{
+	assert(length >= dims[0] * dims[1]);
+	std::ifstream in("STSCI-H-p1936a-f-1024x1024_gray.txt");
+	assert(in.is_open() == true);
+
+	int c;
+	int i = 0;
+	while (in && i < dataLength)
+	{
+		in >> c;
+		//switch(c)
+		//{
+		//case '{':
+		//case '}':
+		//case ',':
+		//case ' ':
+		//	continue;
+		//}
+		//std::cout << "[" << i << "]" << c << std::endl;
+		output[i++] = c;
+	}
+	in.close();
+}
+
+void getSourceArrayDesc(std::vector<pArray>& sourceArr)
+{
+	sourceArr = getSourceArrayDesc();
+}
+
+void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr)
+{
+	if (sourceArr.empty())
+	{
+		sourceArr = getSourceArray();
+	}
+}
+}	// data2D_saturn1024x1024
+
+namespace data2D_solar1024x1024
+{
+std::vector<dim_type> dims = { dimY, dimX };
+std::vector<dim_type> chunkNums = { 8, 8 };
+std::vector<dim_type> chunkDims = { dims[0] / chunkNums[0], dims[1] / chunkNums[1] };
+std::vector<dim_type> blockNums = { 4, 4 };
+std::vector<dim_type> blockDims = { chunkDims[0] / blockNums[0], chunkDims[1] / blockNums[1] };
+
+void getDummy(value_type* output, size_t length)
+{
+	assert(length >= dims[0] * dims[1]);
+	std::ifstream in("latest10240171copy3_1024x1024_gray.txt");
+	assert(in.is_open() == true);
+
+	int c;
+	int i = 0;
+	while (in && i < dataLength)
+	{
+		in >> c;
+		//switch(c)
+		//{
+		//case '{':
+		//case '}':
+		//case ',':
+		//case ' ':
+		//	continue;
+		//}
+		//std::cout << "[" << i << "]" << c << std::endl;
+		output[i++] = c;
+	}
+	in.close();
+}
+
+void getSourceArrayDesc(std::vector<pArray>& sourceArr)
+{
+	sourceArr = getSourceArrayDesc();
+}
+
+void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr)
+{
+	if (sourceArr.empty())
+	{
+		sourceArr = getSourceArray();
+	}
+}
+}	// data2D_solar1024x1024
+
+namespace data2D_mars4096x2048
+{
+std::vector<dim_type> dims = { dimY, dimX };
+std::vector<dim_type> chunkNums = { 8, 8 };
+std::vector<dim_type> chunkDims = { dims[0] / chunkNums[0], dims[1] / chunkNums[1] };
+std::vector<dim_type> blockNums = { 4, 4 };
+std::vector<dim_type> blockDims = { chunkDims[0] / blockNums[0], chunkDims[1] / blockNums[1] };
+
+void getDummy(value_type* output, size_t length)
+{
+	assert(length >= dims[0] * dims[1]);
+	std::ifstream in("Mars_Viking_MDIM21_ClrMosaic_1km_4096x2048_gray.txt");
+	assert(in.is_open() == true);
+
+	int c;
+	int i = 0;
+	while (in && i < dataLength)
+	{
+		in >> c;
+		//switch(c)
+		//{
+		//case '{':
+		//case '}':
+		//case ',':
+		//case ' ':
+		//	continue;
+		//}
+		//std::cout << "[" << i << "]" << c << std::endl;
+		output[i++] = c;
+	}
+	in.close();
+}
+
+void getSourceArrayDesc(std::vector<pArray>& sourceArr)
+{
+	sourceArr = getSourceArrayDesc();
+}
+
+void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr)
+{
+	if (sourceArr.empty())
+	{
+		sourceArr = getSourceArray();
+	}
+}
+}	// data2D_mars4096x2048
 }	// atDummy
 }	// msdb
+
+
+
--- MSDB/MSDB-Test/test/dummy/dummy_astronomy.h
+++ MSDB/MSDB-Test/test/dummy/dummy_astronomy.h
@@ -17,12 +17,10 @@ static const eleType ele_type = eleType::CHAR;
 
 static const dim_type dimX = 1024;
 static const dim_type dimY = 1024;
-//static const dim_type dimX = 4096;
-//static const dim_type dimY = 2048;
 static const size_t dataLength = dimX * dimY;
 static const size_t wtLevel = 3;
 static const size_t mmtLevel = 4;
-static const arrayId aid = 24241;
+static const arrayId aid = 19420;
 
 extern std::vector<dim_type> dims;
 extern std::vector<dim_type> chunkDims;
@@ -55,6 +53,138 @@ std::vector<pArray> getSourceArrayDesc()
 void getSourceArrayDesc(std::vector<pArray>& sourceArr);
 void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr);
 }	// data2D_star1024x1024
+
+namespace data2D_saturn1024x1024
+{
+using value_type = char;
+static const eleType ele_type = eleType::CHAR;
+
+static const dim_type dimX = 1024;
+static const dim_type dimY = 1024;
+static const size_t dataLength = dimX * dimY;
+static const size_t wtLevel = 3;
+static const size_t mmtLevel = 4;
+static const arrayId aid = 19360;
+
+extern std::vector<dim_type> dims;
+extern std::vector<dim_type> chunkDims;
+extern std::vector<dim_type> chunkNums;
+extern std::vector<dim_type> blockDims;
+extern std::vector<dim_type> blockNums;
+
+void getDummy(value_type* output, size_t length);
+
+template<class Aty_ = memBlockArray>
+std::vector<pArray> getSourceArray()
+{
+	// Get Dummy data
+	value_type* data = new value_type[dataLength];
+	getDummy(data, dataLength);
+
+	std::vector<pArray> arrs(
+		{ std::static_pointer_cast<arrayBase>(get2DCharArray<Aty_, value_type>(data, aid, "data2D_saturn1024x1024", dims, chunkDims, blockDims, ele_type)) });
+	return arrs;
+}
+
+template<class Aty_ = memBlockArray>
+std::vector<pArray> getSourceArrayDesc()
+{
+	std::vector<pArray> arrs(
+		{ std::static_pointer_cast<arrayBase>(get2DCharArray<Aty_>(aid, "data2D_saturn1024x1024", dims, chunkDims, blockDims, ele_type)) });
+	return arrs;
+}
+
+void getSourceArrayDesc(std::vector<pArray>& sourceArr);
+void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr);
+}	// data2D_saturn1024x1024
+
+namespace data2D_solar1024x1024
+{
+using value_type = char;
+static const eleType ele_type = eleType::CHAR;
+
+static const dim_type dimX = 1024;
+static const dim_type dimY = 1024;
+static const size_t dataLength = dimX * dimY;
+static const size_t wtLevel = 3;
+static const size_t mmtLevel = 4;
+static const arrayId aid = 10240;
+
+extern std::vector<dim_type> dims;
+extern std::vector<dim_type> chunkDims;
+extern std::vector<dim_type> chunkNums;
+extern std::vector<dim_type> blockDims;
+extern std::vector<dim_type> blockNums;
+
+void getDummy(value_type* output, size_t length);
+
+template<class Aty_ = memBlockArray>
+std::vector<pArray> getSourceArray()
+{
+	// Get Dummy data
+	value_type* data = new value_type[dataLength];
+	getDummy(data, dataLength);
+
+	std::vector<pArray> arrs(
+		{ std::static_pointer_cast<arrayBase>(get2DCharArray<Aty_, value_type>(data, aid, "data2D_solar1024x1024", dims, chunkDims, blockDims, ele_type)) });
+	return arrs;
+}
+
+template<class Aty_ = memBlockArray>
+std::vector<pArray> getSourceArrayDesc()
+{
+	std::vector<pArray> arrs(
+		{ std::static_pointer_cast<arrayBase>(get2DCharArray<Aty_>(aid, "data2D_solar1024x1024", dims, chunkDims, blockDims, ele_type)) });
+	return arrs;
+}
+
+void getSourceArrayDesc(std::vector<pArray>& sourceArr);
+void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr);
+}	// data2D_solar1024x1024
+
+namespace data2D_mars4096x2048
+{
+using value_type = char;
+static const eleType ele_type = eleType::CHAR;
+
+static const dim_type dimX = 1024;
+static const dim_type dimY = 1024;
+static const size_t dataLength = dimX * dimY;
+static const size_t wtLevel = 3;
+static const size_t mmtLevel = 4;
+static const arrayId aid = 40960;
+
+extern std::vector<dim_type> dims;
+extern std::vector<dim_type> chunkDims;
+extern std::vector<dim_type> chunkNums;
+extern std::vector<dim_type> blockDims;
+extern std::vector<dim_type> blockNums;
+
+void getDummy(value_type* output, size_t length);
+
+template<class Aty_ = memBlockArray>
+std::vector<pArray> getSourceArray()
+{
+	// Get Dummy data
+	value_type* data = new value_type[dataLength];
+	getDummy(data, dataLength);
+
+	std::vector<pArray> arrs(
+		{ std::static_pointer_cast<arrayBase>(get2DCharArray<Aty_, value_type>(data, aid, "data2D_mars4096x2048", dims, chunkDims, blockDims, ele_type)) });
+	return arrs;
+}
+
+template<class Aty_ = memBlockArray>
+std::vector<pArray> getSourceArrayDesc()
+{
+	std::vector<pArray> arrs(
+		{ std::static_pointer_cast<arrayBase>(get2DCharArray<Aty_>(aid, "data2D_mars4096x2048", dims, chunkDims, blockDims, ele_type)) });
+	return arrs;
+}
+
+void getSourceArrayDesc(std::vector<pArray>& sourceArr);
+void getSourceArrayIfEmpty(std::vector<pArray>& sourceArr);
+}	// data2D_mars4096x2048
 }	// caDummy
 }	// msdb
 #endif	// _MSDB_DUMMY_ASTRONOMY_H_
