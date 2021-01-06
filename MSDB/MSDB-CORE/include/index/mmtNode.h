feda90e6b418abb4b4a01bc9083b9e286daf2935
RonyK <sangron@naver.com>
2021-01-06 12:31:28 +0900

    Remove vBit in mmtNode

--- MSDB/MSDB-CORE/include/index/mmtNode.h
+++ MSDB/MSDB-CORE/include/index/mmtNode.h
@@ -20,13 +20,13 @@ class mmtNode : public attributeIndex
 public:
 	//using TyType = Ty_;
 
-	sig_bit_type bMax_;			// significant nit of max value
-	sig_bit_type bMin_;			// significant nit of min value
+	sig_bit_type bMax_;			// nth significant of max value
+	sig_bit_type bMin_;			// nth significant of min value
 	bit_cnt_type bMaxDelta_;	// bMax_ delta from a parent node
 	bit_cnt_type bMinDelta_;	// bMin_ delta from a parent node
-	bit_cnt_type order_;		// n th significant bit
+	bit_cnt_type order_;		// n
+	bit_cnt_type childOrder_;	// order for child node
 	bit_cnt_type bits_;			// required bits to represent min/max value
-	bit_cnt_type vBits_;		// required bits to represent value Ty_
 	boost::any max_;
 	boost::any min_;
 	boost::any realMax_;
@@ -41,7 +41,7 @@ public:
 public:
 	mmtNode() : bMax_(0), bMin_(0), bits_(0x80), order_(1),
 		bMaxDelta_(0), bMinDelta_(0), max_(0), min_(0),
-		vBits_(0), realMin_(0), realMax_(0),
+		realMin_(0), realMax_(0),
 		chunkCoor_(1), blockCoor_(1), nodeCoor_(1), seqPos_(0)
 	{
 	}
@@ -82,7 +82,6 @@ public:
 		this->bits_ = sizeof(Ty_) * CHAR_BIT;
 		this->order_ = 1;
 		this->setMinMaxBits<Ty_>();
-		this->vBits_ = msb<Ty_>(this->getMax<Ty_>() - this->getMin<Ty_>()) + 1;
 	}
 
 	template <typename Ty_>
@@ -129,11 +128,11 @@ public:
 		BOOST_LOG_TRIVIAL(debug) << static_cast<int64_t>(this->getMin<Ty_>())
 			<< "(" << static_cast<int64_t>(this->bMin_) << ", " << static_cast<int64_t>(this->bMinDelta_) << ")~"
 			<< static_cast<int64_t>(this->getMax<Ty_>())
-			<< "(" << static_cast<int64_t>(this->bMax_) << ", " << static_cast<int64_t>(this->bMaxDelta_)
-			<< ") / b: " << static_cast<int64_t>(this->bits_)
-			<< " / v: " << static_cast<int64_t>(this->vBits_)
+			<< "(" << static_cast<int64_t>(this->bMax_) << ", " << static_cast<int64_t>(this->bMaxDelta_) << ")"
+			<< " / b: " << static_cast<int64_t>(this->bits_)
+			<< " / bTy: " << static_cast<int64_t>(sizeof(Ty_) * CHAR_BIT)
 			<< " / or: " << static_cast<int64_t>(this->order_)
-			<< "/ real: " << static_cast<int64_t>(this->getRealMin<Ty_>()) << "~" << static_cast<int64_t>(this->getRealMax<Ty_>());
+			<< " / real: " << static_cast<int64_t>(this->getRealMin<Ty_>()) << "~" << static_cast<int64_t>(this->getRealMax<Ty_>());
 	}
 
 	template <typename Ty_>
@@ -144,11 +143,11 @@ public:
 		ss << static_cast<int64_t>(this->getMin<Ty_>())
 			<< "(" << static_cast<int64_t>(this->bMin_) << ", " << static_cast<int64_t>(this->bMinDelta_) << ")~"
 			<< static_cast<int64_t>(this->getMax<Ty_>())
-			<< "(" << static_cast<int64_t>(this->bMax_) << ", " << static_cast<int64_t>(this->bMaxDelta_)
-			<< ") / b: " << static_cast<int64_t>(this->bits_)
-			<< " / v: " << static_cast<int64_t>(this->vBits_)
+			<< "(" << static_cast<int64_t>(this->bMax_) << ", " << static_cast<int64_t>(this->bMaxDelta_) << ")"
+			<< " / b: " << static_cast<int64_t>(this->bits_)
+			<< " / bTy: " << static_cast<int64_t>(sizeof(Ty_) * CHAR_BIT)
 			<< " / or: " << static_cast<int64_t>(this->order_)
-			<< "/ real: " << static_cast<int64_t>(this->getRealMin<Ty_>()) << "~" << static_cast<int64_t>(this->getRealMax<Ty_>()) << "\n"
+			<< " / real: " << static_cast<int64_t>(this->getRealMin<Ty_>()) << "~" << static_cast<int64_t>(this->getRealMax<Ty_>()) << "\n"
 			<< this->chunkCoor_.toString() << " / " << this->blockCoor_.toString() << " / " << this->nodeCoor_.toString();
 
 		return ss.str();
