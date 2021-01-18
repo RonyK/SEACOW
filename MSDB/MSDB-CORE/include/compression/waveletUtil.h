7fd2b7d8a794675d864419bf527a511ef81502e4
RonyK <sangron@naver.com>
2021-01-18 14:31:38 +0900

    Wavelet Util

--- MSDB/MSDB-CORE/include/compression/waveletUtil.h
+++ MSDB/MSDB-CORE/include/compression/waveletUtil.h
@@ -8,5 +8,18 @@
 namespace msdb
 {
 coorRange getBandRange(const size_t bandId, const coor& bandDims);
+
+// Get wavelet band info
+
+struct blockInfo
+{
+	size_t curLevel;
+	size_t curBand;
+};
+
+using pVecBlockInfo = std::shared_ptr<std::vector<blockInfo>>;
+
+pVecBlockInfo getWaveletBlockInfo(size_t maxLevel, size_t dSize, size_t blockId);
+
 }		// msdb
 #endif	// _MSDB_WAVELETUTIL_H_
