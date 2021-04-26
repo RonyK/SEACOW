888bb3ecfa72a661c0f998757fcdd64bbb8fa5f9
RonyK <sangron@naver.com>
2021-04-26 19:27:11 +0900

    Thread Util

--- MSDB/MSDB-CORE/src/util/threadUtil.cpp
+++ MSDB/MSDB-CORE/src/util/threadUtil.cpp
@@ -3,5 +3,30 @@
 
 namespace msdb
 {
+threadUtil::threadUtil()
+	: threadExist_(false), threadNums_(0)
+{
+}
 
+threadUtil::~threadUtil()
+{
+}
+
+void threadUtil::createThread(size_t threadNums)
+{
+	if (this->threadNums_ >= threadNums)
+	{
+		return;
+	}
+
+	this->io_service_ = std::make_shared<boost::asio::io_service>();
+	this->work_ = std::make_shared<boost::asio::io_service::work>(*(this->io_service_));
+
+	for (size_t i = 0; i < threadNums - this->threadNums_; ++i)
+	{
+		this->threadpool_.create_thread(boost::bind(&boost::asio::io_service::run, this->io_service_));
+	}
+	this->threadExist_ = true;
+	this->threadNums_ = threadNums;
+}
 }	// msdb
