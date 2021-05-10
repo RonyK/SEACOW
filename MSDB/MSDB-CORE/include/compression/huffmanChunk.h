8aeaebb619f3e66549c7731365b8f28ee0cad77c
RonyK <sangron@naver.com>
2021-05-10 19:26:56 +0900

    Apply byte unit conversion in Huffman Coding

--- MSDB/MSDB-CORE/include/compression/huffmanChunk.h
+++ MSDB/MSDB-CORE/include/compression/huffmanChunk.h
@@ -28,37 +28,54 @@ private:
 	template<typename Cty_, typename Ty_>
 	void serializeTy(bstream& out)
 	{
-		aHuffmanCoder<Cty_> coder(sizeof(Ty_) * CHAR_BIT);
+		//aHuffmanCoder<Cty_> coder(sizeof(Ty_) * CHAR_BIT);
 
-		auto bit = this->getBlockIterator();
-		while(!bit->isEnd())
+		//auto bit = this->getBlockIterator();
+		//while(!bit->isEnd())
+		//{
+		//	if(bit->isExist())
+		//	{
+		//		pHuffmanBlock hb = std::static_pointer_cast<huffmanBlock>(**bit);
+		//		hb->serializeTy<Cty_, Ty_>(coder, out);
+		//	}
+
+		//	++(*bit);
+		//}
+
+		aHuffmanCoder<uint32_t> coder(sizeof(uint8_t) * CHAR_BIT);
+		auto buffer = (const char*)this->getBuffer()->getReadData();
+		auto size = this->getBuffer()->size();
+		for(int i = 0; i < size; ++i)
 		{
-			if(bit->isExist())
-			{
-				pHuffmanBlock hb = std::static_pointer_cast<huffmanBlock>(**bit);
-				hb->serializeTy<Cty_, Ty_>(coder, out);
-			}
-
-			++(*bit);
+			coder.encode(out, buffer[i]);
 		}
 	}
 
 	template<typename Cty_, typename Ty_>
 	void deserializeTy(bstream& in)
 	{
-		aHuffmanCoder<Cty_> coder(sizeof(Ty_) * CHAR_BIT);
+		//aHuffmanCoder<Cty_> coder(sizeof(Ty_) * CHAR_BIT);
 
-		auto bit = this->getBlockIterator();
-		while (!bit->isEnd())
+		//auto bit = this->getBlockIterator();
+		//while (!bit->isEnd())
+		//{
+		//	if (bit->isExist())
+		//	{
+		//		auto a = *bit;
+		//		pHuffmanBlock hb = std::static_pointer_cast<huffmanBlock>(**bit);
+		//		hb->deserializeTy<Cty_, Ty_>(coder, in);
+		//	}
+
+		//	++(*bit);
+		//}
+
+		aHuffmanCoder<uint32_t> coder(sizeof(uint8_t) * CHAR_BIT);
+		auto buffer = (unsigned char*)this->getBuffer()->getData();
+		auto size = this->getBuffer()->size();
+		for(int i = 0; i < size; ++i)
 		{
-			if (bit->isExist())
-			{
-				auto a = *bit;
-				pHuffmanBlock hb = std::static_pointer_cast<huffmanBlock>(**bit);
-				hb->deserializeTy<Cty_, Ty_>(coder, in);
-			}
-
-			++(*bit);
+			unsigned char value = coder.decode(in);
+			buffer[i] = value;
 		}
 	}
 };
